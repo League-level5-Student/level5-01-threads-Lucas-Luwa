72cd50f61640e07770a4108d01a5153d56d922f7
Luki <>
2019-03-13 18:25:57 -0700

    Save 2
    3/13/19

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -2,9 +2,13 @@ package _04_Thread_Pool;
 
 public class ThreadPool {
 
-	public void addTask(Object object) {
-		// TODO Auto-generated method stub
+	public ThreadPool(int i) {
+		// TODO Auto-generated constructor stub
+	}
 
+	public void start() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
